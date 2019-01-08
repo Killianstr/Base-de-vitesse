61e7264ca4e815148f82ef9fa5b3c924d42a29a6
AdamFaes <faes@lacroixrouge-brest.fr>
2019-01-08 10:34:54 +0100

    Ajout de Bootsrap

--- Adam/index.php
+++ Adam/index.php
@@ -1,1 +1,28 @@
-test
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Projet Robot</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+</head>
+<body>
+    <ul>
+        <li>Contrôle</li>
+        <li>Administration</li>
+        <li>Aide</li>
+    </ul>
+    <h3>Panneau de contrôle</h3>
+    <p>État du Robot : <strong>OFF</strong></p>
+    <div align="center">
+            <h3>Contrôle des mouvements : </h3>
+    </div>
+    <div align="center">
+            <a href="#" class="boutonAvancer">Avancer</a>
+            <a href="#" class="boutonAvancer">Gauche</a>
+            <a href="#" class="boutonAvancer">Droite</a>
+            <a href="#" class="boutonAvancer">Reculer</a>
+          </div> 
+    
+</body>
+</html>
