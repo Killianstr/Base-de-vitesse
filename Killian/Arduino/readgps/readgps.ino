0afae1bb05de7dc40d3a36ee777d5d37f5dbb1b9
Killianstr <stricot.killian@gmail.com>
2019-04-30 11:37:36 +0200

    mise à jour programme

--- Killian/Arduino/readgps/readgps.ino
+++ Killian/Arduino/readgps/readgps.ino
@@ -1,5 +1,6 @@
-//parseSerial pour trier le string en tableau de string separé par les ","
-
+String tabdata[15];
+int index,idx;
+String trameGGA,data;
 void setup() {
   // initialize both serial ports:
   Serial1.begin(9600);
@@ -7,57 +8,37 @@ void setup() {
   while (!Serial1) {
      ;
   };
-//  Serial1.println("this is serial");
-//  Serial.println("this is serial1");
+  Serial.println("this is serial");
+
 }
-
-String lattitude; 
-String longitude;
-String vitesseN;
-String vitesseK;
-int idx=0;
-
 void loop() {
   // read from port 1, send to port 0:
   if (Serial.available()) {
-    char inByte = Serial.read();
-  //  Serial1.write(inByte);
-    if (inByte = "$GGA"){
-        while (idx < 14 )
-          {
-            if (inByte == ',') {idx++;} 
-              if (idx == 2)
-                
-              {
-                while(idx<4)
-                  {lattitude+=inByte;}
-              } 
-            if (inByte == 4)
-            {
-              while (idx<6)
-                {longitude+=inByte;}
-            }
-          }      
-      } 
-  
-    if (inByte = "$GPVTG"){
-      if ((idx >= 5) and (idx <=6))
-        {vitesseN += inByte;}
-      if ((idx >= 7) and (idx <=8))
-        {vitesseK += inByte;}        
-    }
+    int inByte = Serial.read();
+    Serial1.write(inByte);
   }
-
-   
-  // read from port 0, send to port 1:
-  //if (Serial1.available()) {
- //   char inByte = Serial1.read();
-    Serial.println(longitude);
-    Serial.println(lattitude);
-    Serial.println(vitesseN);
-    Serial.println(vitesseK);
-
-    
-    //  Serial.write(vitesse);
- //}
+  
+  if (Serial1.available()) { 
+     data = Serial1.readStringUntil('\n');
+    if (data.startsWith("$GPGGA") == true){
+          Serial.println("GGA TROUVE!!!!!");
+          trameGGA=data.substring('\n');
+          Serial.println(trameGGA);
+          index=trameGGA.indexOf(',');
+          tabdata[0]=trameGGA.substring(0,index);
+          tabdata[1]=trameGGA.substring(index+1,trameGGA.length());
+          Serial.println(tabdata[0]);                              
+          Serial.println(tabdata[1]);
+      for(idx=1;idx<14;idx++)
+        {   
+            index=tabdata[idx].indexOf(',');        
+            tabdata[idx+1]=tabdata[idx].substring(index+1,tabdata[1].length());
+            tabdata[idx]=tabdata[idx].substring(0,index);
+        }       
+    for(int id=0;id<14;id++) 
+       {Serial.println(tabdata[id]);}                              
+     
+       
+  }
+  }
 }
