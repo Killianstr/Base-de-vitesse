a22fe8aedd309704a7d0856607ce0457e7687e25
Killianstr <44776909+Killianstr@users.noreply.github.com>
2019-06-06 14:29:56 +0200

    Add files via upload
    Fonction pour trier les trames NMEA

--- Killian/Arduino/readgps/readgps.ino
+++ Killian/Arduino/readgps/readgps.ino
@@ -1,57 +1,53 @@
 String tabdata[15];
 int index, idx;
-String trameGGA, data;
+String trameRMC,data,datatosend;
 String temp;
+float vitessekm;
 void setup() {
-  // initialize both serial ports:
+
   Serial1.begin(9600);
   Serial.begin(9600);
   while (!Serial1) {
     ;
   };
   Serial.println("this is serial");
-
 }
+
+void triTrameNMEA(String Start,String trame,int nbindex,String *tableau) //fonction qui extrait une trame de la trame NMEA et trie la donnee dans un tableau
+{
+    if (data.startsWith("$GP"+Start) == true) //trouve la ligne qui commence par $GPRMC
+     { 
+       index = trame.indexOf(',');
+       tableau[0] = trame.substring(0, index);
+       temp = trame.substring(index + 1, trame.length());
+       for(idx=1;idx<nbindex+1;idx++)
+          {
+             index = temp.indexOf(',');
+             if (index != 0)
+                tableau[idx] = temp.substring(0, index);
+             else
+                tableau[idx] = "";       
+             temp = temp.substring(index + 1, temp.length()); 
+           }
+     }
+}
+
 void loop() {
-  // read from port 1, send to port 0:
   if (Serial.available()) {
-    int inByte = Serial.read();
+    int inByte = Serial.read();  //met en écoute le port 0 et écrit sur le port 1
     Serial1.write(inByte);
   }
 
   if (Serial1.available()) {
-    data = Serial1.readStringUntil('\n');
-    if (data.startsWith("$GPGGA") == true) {
-      Serial.println("GGA TROUVE!!!!!");
-      trameGGA = data.substring('\n');
-      Serial.println(trameGGA);
-      index = trameGGA.indexOf(',');
-      tabdata[0] = trameGGA.substring(0, index);
-      temp = trameGGA.substring(index + 1, trameGGA.length());
-      Serial.println(tabdata[0]);
-      Serial.println(tabdata[1]);
-
-      for (idx = 1; idx < 14; idx++)
-      {
-        index = temp.indexOf(',');
-        if (index != 0)
-          tabdata[idx] = temp.substring(0, index);
-        else
-          tabdata[idx] = "";
-          
-        temp = temp.substring(index + 1, temp.length());
-        Serial.print("temp");
-        Serial.print(idx);
-        Serial.print(": ");
-        Serial.println(temp);
-
-      }
-      for (int id = 0; id < 14; id++)
-      {
-        Serial.println(tabdata[id]);
-      }
-
-
+      data = Serial1.readStringUntil('\n');      //détecte le fin de ligne et stocke tout ce qu'il y a avant
+      Serial.println(data);
+      trameRMC = data.substring('\n');
+      Serial.println(trameRMC);
+      triTrameNMEA("RMC",trameRMC,12,tabdata);
+      vitessekm=(tabdata[6].toFloat()*1.852);           //calcul vitesse en noeud
+      datatosend=tabdata[2]+tabdata[3]+tabdata[4]+'|'+tabdata[5]+'|'+tabdata[6]+'|'+vitessekm+'|'+tabdata[12];
+      Serial.println(datatosend);                      // Longitude|Lattitude|VitesseNoeuds|VitesseKm/h
     }
   }
-}
+  
+  
